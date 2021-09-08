ef686d78dfc8fefc7c00a686c7ec433552d96737
Yi Joon Choi <69078056+dlwns33@users.noreply.github.com>
2021-09-08 11:27:37 +0900

    ✨[FEAT] : YiJoonMemo struct

--- Basic-Exercise/Rx/Source/Models/YiJoonMemo.swift
+++ Basic-Exercise/Rx/Source/Models/YiJoonMemo.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct YiJoonMemo : Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date())
+    {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: YiJoonMemo, updateContent: String)
+    {
+        self = original
+        self.content = updateContent
+    }
+}
